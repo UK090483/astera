18e8ee9a7f5cb2bb1488ff73adefd76f5e118bd4
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-11-23 13:21:22 +0100

    no cdn ??

--- lib/SanityService/config.tsx
+++ lib/SanityService/config.tsx
@@ -2,6 +2,6 @@ export const config = {
   dataset: process.env.SANITY_PROJECT_DATASET || "",
   projectId: process.env.SANITY_PROJECT_ID || "",
   // useCdn: process.env.NODE_ENV === "production",
-  useCdn: false,
+  useCdn: true,
   apiVersion: "2021-10-21",
 };
