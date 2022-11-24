bc26173e3d0731cf8292576a6c085ec91fec257c
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-11-24 11:41:20 +0100

    cleaning imports

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,11 +8,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" href="/favicon.ico" />
-
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-            rel="stylesheet"
-          />
         </Head>
 
         <body
