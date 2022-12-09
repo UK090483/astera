9c33f4e6c35873239655dc28dba00c7deb3a2bc9
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-12-09 14:42:16 +0100

    add type onPageLink

--- PageBuilder/Objects/link/link.query.ts
+++ PageBuilder/Objects/link/link.query.ts
@@ -4,11 +4,10 @@ import { localizedQueryFn } from "PageBuilder/helper/withLocalization";
 export type linkResult = {
   internal?: string;
   href?: string;
-  onPageLink?: string;
 };
 
 const linkProjection: localizedQueryFn = (locale) => `
-...(internal->{ 'internal':${SLUG_PROJECTION(locale)} }),
+...(internal->{ 'internal':${SLUG_PROJECTION(locale)}  }),
 onPageLink,
 href,
 `;
