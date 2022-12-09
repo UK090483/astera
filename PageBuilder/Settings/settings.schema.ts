688365bc6295a07ae5e1a2c6cb4ac0004c083f74
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-12-09 14:42:35 +0100

    add default hero image

--- PageBuilder/Settings/settings.schema.ts
+++ PageBuilder/Settings/settings.schema.ts
@@ -33,6 +33,53 @@ export const SettingsDocument: DocumentDefinition = {
       title: "Default Hero image",
       type: "defaultImage",
     },
+
+    {
+      name: "defaultPersonHero",
+      title: "Default Hero image Person",
+      type: "defaultImage",
+    },
+    {
+      name: "personListingPage",
+      title: "Person Listing Page",
+      type: "object",
+      fields: [
+        {
+          name: "page",
+          type: "reference",
+          to: [{ type: "page" }],
+        },
+        {
+          name: "onPageLink",
+          title: "On page Link",
+          description: "must be the exact Title of a Section",
+          type: "string",
+        },
+      ],
+    },
+    {
+      name: "defaultNewsHero",
+      title: "Default Hero image News",
+      type: "defaultImage",
+    },
+    {
+      name: "heroListingPage",
+      title: "Hero Listing Page",
+      type: "object",
+      fields: [
+        {
+          name: "page",
+          type: "reference",
+          to: [{ type: "page" }],
+        },
+        {
+          name: "onPageLink",
+          title: "On page Link",
+          description: "must be the exact Title of a Section",
+          type: "string",
+        },
+      ],
+    },
   ],
   preview: {
     prepare(selection: any) {
