6c7e9eb2bbedae30d2fbab9139a48ab7fbe3fd91
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-12-12 14:50:21 +0100

    add 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,23 +1,82 @@
 import * as React from "react";
 
-import Image from "next/legacy/image";
+import Image from "next/image";
+import Image404 from "../public/images/404.png";
+import Section from "@components/Section/Section";
+import { sanityClient } from "@lib/SanityService/sanity.server";
+import { GetStaticProps } from "next";
+import { footerQuery } from "@components/Layout/Footer/Footer.query";
+import navigationQuery from "PageBuilder/Navigation/navigation.query";
+import { IMAG_PROJECTION } from "PageBuilder/constants";
+import BodyParser from "PageBuilder/lib/BodyParser";
+import HeroBlock from "PageBuilder/Components/Hero/frontend/HeroBlock";
+import Link from "next/link";
+import useButtonStyle from "@components/Button/useButtonStyle";
+import { useRouter } from "next/router";
+import useTranslation from "@hooks/useTranslation";
 
-interface IStypeProps {}
+const Page404: React.FunctionComponent = () => {
+  const buttonStyle = useButtonStyle();
 
-const Page404: React.FunctionComponent<IStypeProps> = (props) => {
+  const { notFound, sorry, home, text } = useTranslation({
+    notFound: { default: "SEITE NICHT GEFUNDEN!", en: "PAGE NOT FOUND!" },
+    sorry: {
+      default:
+        "Es tut uns leid, aber wir können die gesuchte Seite nicht finden.",
+      en: "Sorry, we can’t seem to find the page you’re looking for.",
+    },
+    text: {
+      default:
+        "Möglicherweise ist in der eingegebenen URL ein Schreibfehler enthalten, oder die gesuchte Seite existiert nicht mehr.",
+      en: "There may be a misspelling in the URL entered, or the page you are looking for may no longer exist.",
+    },
+    home: {
+      default: "Zur Startseite",
+      en: "Back to Home",
+    },
+  });
+
   return (
-    <div className="relative h-screen flex w-full justify-center items-center ">
-      <div className=" w-full  max-w-lg  ">
+    <>
+      <BodyParser
+        components={{
+          hero: {
+            component: HeroBlock,
+          },
+        }}
+      />
+      <Section width="s" topSpace="l" bottomSpace="xl">
+        <div className="header1  text-right pr-16 ">OOOOPS!</div>
         <Image
-          src="/images/404_Page.png"
-          width={600}
-          height={600}
-          alt={"man on blackboard with 404 written on"}
-          layout="responsive"
+          width={700}
+          src={Image404}
+          className="mx-auto"
+          alt={"404 in written text"}
         />
-      </div>
-    </div>
+
+        <h1 className="header1 pt-8">{notFound}</h1>
+        <p>{sorry}</p>
+        <p>{text}</p>
+
+        <Link className={"mt-12 " + buttonStyle} href="/">
+          {home}
+        </Link>
+      </Section>
+    </>
   );
+};
+
+const Page404Query = (locale?: string) => `
+{
+'body':[{'_key':'404', '_type':'hero','image': *[_id=='baseConfig'][0].defaultHero{${IMAG_PROJECTION}}}],
+${navigationQuery(locale)}
+${footerQuery(locale)}
+}
+`;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await sanityClient.fetch(Page404Query(context.locale));
+  return { props: { data: res }, revalidate: 1 };
 };
 
 export default Page404;
