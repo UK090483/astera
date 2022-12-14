4124d9f63338ab3770a5d1ea51504b78cdb498a8
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-12-14 10:07:35 +0100

    fix arrow position

--- components/ArrowIcon.tsx
+++ components/ArrowIcon.tsx
@@ -4,7 +4,7 @@ const ArrowIcon: React.FC<{ className?: string }> = (props) => {
     <svg
       viewBox="0 0 17 12"
       xmlns="http://www.w3.org/2000/svg"
-      className={" inline " + className}
+      className={"" + className}
     >
       <path d="M12.1713 11.8741L16.1713 6.87408L16.9209 5.93704L16.1713 5L12.1713 0L9.82869 1.87409L13.0791 5.93704L9.82869 10L12.1713 11.8741ZM1.14441e-05 7.4978L9.00001 7.4978L9.00001 4.4978L1.14441e-05 4.4978V7.4978Z" />
     </svg>
